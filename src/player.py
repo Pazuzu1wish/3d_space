d347bc5d9425236f3e8e6f5995acf4f4647c0ee8
tony <pazuzu1wish@gmail.com>
2026-05-25 22:38:05 -0400

    feat: implement missile firing logic and update save data with recorded match statistics

--- src/player.py
+++ src/player.py
@@ -454,6 +454,7 @@ class Player:
         fire_lasers(self, fire_pressed, handler, lasers, sound)
         fire_missile(self, missile_fire_pressed, handler, player_missiles, sound)
         
+        
         # ── ENGINE TRAIL SPAWNING & UPDATING ──────────
         fx, fy, fz = get_forward_from_quat(self.orientation)
         _, right, up = get_basis_from_quat(self.orientation)
